76dbb8ba2f5db2744a12a61b489a04ce09cd8aba
achenson <45999022+Achenson@users.noreply.github.com>
2019-06-18 16:17:13 +0200

    Updates

---
+++ precache-manifest.da0f2d990adfa0f9f96f479c84c07987.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c8341d9be380be53cdd8a8b3df70da",
+    "url": "/calculator-react/index.html"
+  },
+  {
+    "revision": "f9ec10d677fdd45e3ecd",
+    "url": "/calculator-react/static/css/main.a4f6db13.chunk.css"
+  },
+  {
+    "revision": "283cbb3238da73526ab7",
+    "url": "/calculator-react/static/js/2.21e67013.chunk.js"
+  },
+  {
+    "revision": "f9ec10d677fdd45e3ecd",
+    "url": "/calculator-react/static/js/main.c74b0acf.chunk.js"
+  },
+  {
+    "revision": "823ceae628c968c44b0a",
+    "url": "/calculator-react/static/js/runtime~main.b9732b27.js"
+  }
+]);
